8e269b1a0e4333943ed16fb82087e4ea9c4d09ea
SvetlanaB <bsv211112@gmail.com>
2022-09-17 16:27:23 +0300

    Example 010 Method Array added

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {10, 20, 35, 4, 35, 56, 47, 8};
+
+int n = array.Length;
+int find = 47;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index);
+    }
+    index++;
+}
